1109d207b509e50d0322189d725ad086858fcefc
HeyyUmerr <96136178+HeyyUmerr@users.noreply.github.com>
2022-04-05 13:55:03 +0530

    Create linearsearchpositin.py

---
+++ linearsearchpositin.py
@@ -0,0 +1,15 @@
+array = [89,64,64,23,10,78,67,78,64,23]
+def Linear_search(array,element):
+    for i in range(len(array)):
+        if (array[i] == element):
+         index.append(i)
+         print(index)
+         
+      
+      
+     
+    
+         
+index  = []    
+element = int(input())
+Linear_search(array,element)
